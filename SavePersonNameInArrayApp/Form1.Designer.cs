b12b143afc0a40ebc886f9e68ff8c8a47b5530b2
s-sabbir <dew.fog1553@gmail.com>
2015-06-03 01:29:24 +0600

    Initial Commit for SavePersonNameInArrayApp

---
+++ SavePersonNameInArrayApp/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SavePersonNameInArrayApp
+{
+    partial class SavePersonNameInArrayUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.showAllListBox = new System.Windows.Forms.ListBox();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.TotalLabel = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(165, 28);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(162, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(76, 31);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(63, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Enter Name";
+            // 
+            // showAllListBox
+            // 
+            this.showAllListBox.FormattingEnabled = true;
+            this.showAllListBox.Location = new System.Drawing.Point(79, 62);
+            this.showAllListBox.Name = "showAllListBox";
+            this.showAllListBox.Size = new System.Drawing.Size(248, 134);
+            this.showAllListBox.TabIndex = 4;
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.Location = new System.Drawing.Point(165, 215);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.Size = new System.Drawing.Size(161, 20);
+            this.totalTextBox.TabIndex = 5;
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.AutoSize = true;
+            this.TotalLabel.Location = new System.Drawing.Point(76, 218);
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(31, 13);
+            this.TotalLabel.TabIndex = 4;
+            this.TotalLabel.Text = "Total";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(361, 28);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(361, 62);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show All";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(361, 101);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // SavePersonNameInArrayUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 306);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.TotalLabel);
+            this.Controls.Add(this.totalTextBox);
+            this.Controls.Add(this.showAllListBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "SavePersonNameInArrayUI";
+            this.Text = "Save Person\'s Name In Array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.ListBox showAllListBox;
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.Label TotalLabel;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ SavePersonNameInArrayApp/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SavePersonNameInArrayApp
+{
+    public partial class SavePersonNameInArrayUI : Form
+    {
+        int count = 0, previousIndex;
+        bool loop = true;
+        string[] nameArray = new string[5];
+        public SavePersonNameInArrayUI()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+
+            if (count < nameArray.Length)
+            {
+                string name = nameTextBox.Text;
+                nameArray[count] = name;
+                previousIndex = count++;
+                loop = true;
+                nameTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("U Can store only 5 names...");
+                nameTextBox.Clear();
+            }
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            if (loop == true)
+            {
+                showAllListBox.Items.Add(nameArray[previousIndex]);
+                totalTextBox.Text = count.ToString();
+            }
+            loop = false;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
